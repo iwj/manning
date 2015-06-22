6cdc42332322aa9b1174d70397e6861a894c08f4
iwj <jentlewoo@gmail.com>
2015-06-23 00:07:00 +0800

    习题

---
+++ chapter02/if_26h_every_day.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+"""
+人们总说没有足够的时间做到尽善尽美。如果一天有26个小时，那么一周会有多少分钟？
+"""
+
+weekday = 7
+dayhour = 26
+hourminute = 60
+
+result = weekday * dayhour * hourminute
+
+print result
