591ece49e6761fc7001147f802e94a9d7fd4d349
iwj <jentlewoo@gmail.com>
2015-08-11 14:24:46 +0800

    ch12 list part

---
+++ chapter12/2015-08-11_ipython.py
@@ -0,0 +1,138 @@
+In [7]: del family[2]
+
+In [8]: family
+Out[8]: ['Mon', 'Dad', 'Baby', 'Davaid']
+
+In [9]: a = family.pop(0)
+
+In [10]: a
+Out[10]: 'Mon'
+
+In [11]: type(a)
+Out[11]: str
+
+In [12]: family
+Out[12]: ['Dad', 'Baby', 'Davaid']
+
+In [13]: if "Dad" in f
+%%file     family     file       filter     finally    float      for        format     from       frozenset
+
+In [13]: if "Dad" in family:
+   ....:     print "Dad在列表里"
+   ....: else：
+  File "<ipython-input-13-1b0852c6a6cb>", line 3
+    else：
+        ^
+SyntaxError: invalid syntax
+
+
+In [14]: if "Dad" in family:
+    print "Dad在列表里"
+   ....:
+Dad在列表里
+
+In [15]: "Dad" in family
+Out[15]: True
+
+In [16]: "Dad1" in family
+Out[16]: False
+
+In [17]: family.index("Dad")
+Out[17]: 0
+
+In [18]: family.index("Baby")
+Out[18]: 1
+
+In [19]:
+
+In [19]:
+
+In [19]:
+
+In [19]: original = [1, 3, 5, 2, 4]
+
+In [20]: or
+or        ord       original
+
+In [20]: or
+  File "<ipython-input-20-072c1a5cfd6b>", line 1
+    or
+     ^
+SyntaxError: invalid syntax
+
+
+In [21]: original
+Out[21]: [1, 3, 5, 2, 4]
+
+In [22]: new = or
+or        ord       original
+
+In [22]: new = original
+
+In [23]: new,or
+or        ord       original
+
+In [23]: new,original
+Out[23]: ([1, 3, 5, 2, 4], [1, 3, 5, 2, 4])
+
+In [24]: original.sort()
+
+In [25]: original
+Out[25]: [1, 2, 3, 4, 5]
+
+In [26]: new
+Out[26]: [1, 2, 3, 4, 5]
+
+In [27]: or
+or        ord       original
+
+In [27]: original.append(2)
+
+In [28]: or
+or        ord       original
+
+In [28]: original
+Out[28]: [1, 2, 3, 4, 5, 2]
+
+In [29]: original.remove(2)
+
+In [30]: original
+Out[30]: [1, 3, 4, 5, 2]
+
+In [31]: new = o
+object    oct       open      or        ord       original
+
+In [31]: new = or
+or        ord       original
+
+In [31]: new = original[:]
+
+In [32]: new
+Out[32]: [1, 3, 4, 5, 2]
+
+In [33]: original
+Out[33]: [1, 3, 4, 5, 2]
+
+In [34]: new.sort
+Out[34]: <function sort>
+
+In [35]: new.sort()
+
+In [36]: new
+Out[36]: [1, 2, 3, 4, 5]
+
+In [37]: original
+Out[37]: [1, 3, 4, 5, 2]
+
+In [38]: new = original[:]
+
+In [39]: new, original
+Out[39]: ([1, 3, 4, 5, 2], [1, 3, 4, 5, 2])
+
+In [40]: original.sort()
+
+In [41]: new , original
+Out[41]: ([1, 3, 4, 5, 2], [1, 2, 3, 4, 5])
+
+In [42]: new, original
+Out[42]: ([1, 3, 4, 5, 2], [1, 2, 3, 4, 5])
